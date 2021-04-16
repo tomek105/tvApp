95587c7fe95c1cc2aa86d19420e709173795d6da
Tomasz <lisssu14@gmail.com>
2021-04-16 17:25:37 +0200

    Displaying shows on list

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
-import { mapListToDOMElements } from "./domInteractions.js";
+import {
+    mapListToDOMElements,
+    createDOMElement
+} from "./domInteractions.js";
+import {
+    getShowsByKey
+} from "./requests.js";
 class TVapp {
     constructor() {
         this.viewElems = {};
@@ -6,9 +12,11 @@ class TVapp {
         this.selectedName = "harry";
         this.initializeApp();
     }
+
     initializeApp = () => {
         this.connectDOMElements();
         this.setupListeners();
+        this.fetchAndDisplayShows();
     }
 
     connectDOMElements = () => {
@@ -19,19 +27,56 @@ class TVapp {
         console.log(listOfShowNames);
 
         this.viewElems = mapListToDOMElements(listOfIds, 'id');
-        this.showNamesButtons = mapListToDOMElements(listOfShowNames,'data-show-name');
+        this.showNamesButtons = mapListToDOMElements(listOfShowNames, 'data-show-name');
 
         console.log(this.viewElems);
         console.log(this.showNamesButtons);
     }
 
-    setupListeners = () =>{
+    setupListeners = () => {
         Object.keys(this.showNamesButtons).forEach(showName => {
             this.showNamesButtons[showName].addEventListener("click", this.setCurrentNameFilter);
         })
     }
-    setCurrentNameFilter = () =>{
+
+    setCurrentNameFilter = () => {
         this.selectedName = event.target.dataset.showName;
+        this.fetchAndDisplayShows();
     }
+
+    fetchAndDisplayShows = () => {
+        getShowsByKey(this.selectedName).then(shows => this.renderCards(shows));
+    }
+
+    renderCards = shows => {
+        for (const { show } of shows) {
+            this.createShowCard(show);
+        }
+    }
+
+    createShowCard = show => {
+
+        const divCard = createDOMElement('div', 'card');
+        const divCardBody = createDOMElement('div', 'card-body');
+        const h5 = createDOMElement('h5', 'card-title', show.name);
+        const p = createDOMElement('p', 'card-text', show.summary);
+        const btn = createDOMElement('button', 'btn btn-primary', 'Show more');
+        let img;
+        if(show.image){
+             img = createDOMElement('img', 'card-img-top', null, show.image.medium);
+        }
+        else{
+             img = createDOMElement('img', 'card-img-top', null, 'https://via.placeholder.com/210x295');
+        }
+
+        divCard.appendChild(divCardBody);
+        divCardBody.appendChild(img);
+        divCardBody.appendChild(h5);
+        divCardBody.appendChild(p);
+        divCardBody.appendChild(btn);
+        this.viewElems.showsWrapper.appendChild(divCard);
+    }
+
+
 }
 document.addEventListener("DOMContentLoaded", new TVapp());
--- src/domInteractions.js
+++ src/domInteractions.js
@@ -1,14 +1,27 @@
 const _getDOMElem = (attribute, value) => {
-    return document.querySelector(`[${attribute} = "${value}"]`);
+  return document.querySelector(`[${attribute} = "${value}"]`);
+}
+
+export const mapListToDOMElements = (listOfValues, attribute) => { //
+  const _viewElems = {};
+
+  for (const value of listOfValues) {
+    _viewElems[value] = _getDOMElem(attribute, value);
   }
-  
-  export const mapListToDOMElements = (listOfValues, attribute) => { //
-    const _viewElems = {};
-  
-    for (const value of listOfValues) {
-      _viewElems[value] = _getDOMElem(attribute, value);
-    }
-  
-    return _viewElems;
+
+  return _viewElems;
+}
+
+export const createDOMElement = (tagName, className, innerText, src) => {
+  const tag = document.createElement(tagName);
+  tag.classList = className;
+
+  if(innerText){
+    tag.innerText = innerText;
   }
-  
+
+  if(src){
+    tag.src = src;
+  }
+  return tag;
+}
