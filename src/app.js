5f31960ab347493cb02aee7beedf6d1098cf8968
Tomasz <lisssu14@gmail.com>
2021-04-16 18:03:42 +0200

    Added error handling, when a show doesn't have an specific element and clearing the display from the old shows

--- src/app.js
+++ src/app.js
@@ -49,24 +49,29 @@ class TVapp {
     }
 
     renderCards = shows => {
+        this.viewElems.showsWrapper.innerHTML= "";
         for (const { show } of shows) {
             this.createShowCard(show);
         }
     }
 
     createShowCard = show => {
-
         const divCard = createDOMElement('div', 'card');
         const divCardBody = createDOMElement('div', 'card-body');
         const h5 = createDOMElement('h5', 'card-title', show.name);
-        const p = createDOMElement('p', 'card-text', show.summary);
         const btn = createDOMElement('button', 'btn btn-primary', 'Show more');
-        let img;
-        if(show.image){
-             img = createDOMElement('img', 'card-img-top', null, show.image.medium);
+        let img, p;
+
+        if (show.image) {
+            img = createDOMElement('img', 'card-img-top', null, show.image.medium);
+        } else {
+            img = createDOMElement('img', 'card-img-top', null, 'https://via.placeholder.com/210x295');
         }
-        else{
-             img = createDOMElement('img', 'card-img-top', null, 'https://via.placeholder.com/210x295');
+
+        if (show.summary) {
+            p = createDOMElement('p', 'card-text', `${show.summary.slice(0, 80)}...`);
+        } else {
+            p = createDOMElement('p', 'card-text', 'There is no any description for that show yet.');
         }
 
         divCard.appendChild(divCardBody);
